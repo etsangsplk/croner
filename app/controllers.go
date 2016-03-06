581a34518ae8a7122956ee3a036b49bd8bd76144
Raphael Simon <raphael@rightscale.com>
2016-03-06 11:15:34 -0800

    Latest goagen

--- app/controllers.go
+++ app/controllers.go
@@ -29,12 +29,12 @@ func initService(service *goa.Service) {
 	inited = true
 
 	// Setup encoders and decoders
-	service.Encoder(goa.NewXMLEncoder, "application/xml")
-	service.Encoder(goa.NewGobEncoder, "application/gob", "application/x-gob")
 	service.Encoder(goa.NewJSONEncoder, "application/json")
+	service.Encoder(goa.NewGobEncoder, "application/gob", "application/x-gob")
+	service.Encoder(goa.NewXMLEncoder, "application/xml")
 	service.Decoder(goa.NewJSONDecoder, "application/json")
-	service.Decoder(goa.NewXMLDecoder, "application/xml")
 	service.Decoder(goa.NewGobDecoder, "application/gob", "application/x-gob")
+	service.Decoder(goa.NewXMLDecoder, "application/xml")
 
 	// Setup default encoder and decoder
 	service.Encoder(goa.NewJSONEncoder, "*/*")
